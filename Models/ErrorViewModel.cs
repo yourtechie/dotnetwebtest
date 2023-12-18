ac553fc1ea39976e33d24d3a81116aa95e6f2435
Brookeycutie <brookeyajayi55@gmail.com>
2023-12-18 14:45:17 +0100

    Added readme n MVC file

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace dotnet_hello_world.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
