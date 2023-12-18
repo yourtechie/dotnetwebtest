a9bbc85a2c023b45b1d427674ed25210809031ce
Brookeycutie <brookeyajayi55@gmail.com>
2023-12-18 14:31:39 +0100

    I am restarting from scratch

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace dotnet_hello_world.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
